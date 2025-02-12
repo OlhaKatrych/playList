65781f54ec393dfc9c901c3f06a22854df1effd4
OlhaKatrych <olgak198931@gmail.com>
2025-02-12 21:31:40 +0200

    correction

--- js/getDataFromJASON.js
+++ js/getDataFromJASON.js
@@ -2,18 +2,19 @@ import { sortedSongs } from "./helper.js";
 
 async function fetchData() {
   const response = await fetch("../array.json");
+
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
   const data = await response.json();
-  return sortedSongs([...data]);
+  console.log(data);
+  return data;
 }
-
 export async function getDataSong() {
   const songs = await fetchData();
   console.log(songs);
   return {
-    songs,
+    songs: sortedSongs([...songs]),
     currentSong: null,
     songCurrentTime: 0,
   };
