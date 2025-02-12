f203e699c61ad349734060756fe3dd4bad66a62e
OlhaKatrych <olgak198931@gmail.com>
2025-02-12 18:31:13 +0200

    json

--- js/dataSong.js
+++ js/dataSong.js
@@ -1,7 +1,8 @@
-import data from "./array.json" assert { type: 'json' };
+import data from "./array.json";
+console.log(data)
 import { sortedSongs } from "./helper.js";
 export let dataSong = {
-    songs: sortedSongs([...data]),
-    currentSong: null,
-    songCurrentTime: 0,
-  };
+  songs: sortedSongs([...data]),
+  currentSong: null,
+  songCurrentTime: 0,
+};
