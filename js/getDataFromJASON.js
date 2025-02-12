00d3b4fdd8ce2bb364734dd48335f3dba663aaa0
OlhaKatrych <olgak198931@gmail.com>
2025-02-12 21:41:51 +0200

    correction again

--- js/getDataFromJASON.js
+++ js/getDataFromJASON.js
@@ -1,7 +1,7 @@
 import { sortedSongs } from "./helper.js";
 
 async function fetchData() {
-  const response = await fetch("/array.json");
+  const response = await fetch("https://olhakatrych.github.io/playList/array.json");
 
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
