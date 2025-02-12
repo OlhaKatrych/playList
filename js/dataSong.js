b54388061405881ed2fade70576dde18aa68989d
OlhaKatrych <olgak198931@gmail.com>
2025-02-12 19:58:58 +0200

    array json

--- js/dataSong.js
+++ js/dataSong.js
@@ -1,6 +1,6 @@
 import { sortedSongs } from "./helper.js";
-import data from './array.json' with {type: 'json'};
-export let dataSong = {
+import data from "./array.json";
+export const dataSong = {
   songs: sortedSongs([...data]),
   currentSong: null,
   songCurrentTime: 0,
