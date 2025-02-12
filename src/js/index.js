27a8bd1ddf857d3ab935ce674ccd5fbdddaebe3b
OlhaKatrych <olgak198931@gmail.com>
2025-02-12 15:58:10 +0200

    playlist files

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,11 @@ const playButton = document.getElementById("play");
 
 createMurkup(dataSong.songs);
 playButton.addEventListener("click", () => {
+  
   if (dataSong.currentSong === null) {
     playSong(dataSong.songs[0].id);
   } else {
     playSong(dataSong.currentSong.id);
   }
+  playButton.classList.add("playing");
 });
--- src/js/playSong.js
+++ src/js/playSong.js
@@ -5,13 +5,12 @@ export const playSong = (id) => {
   const song = dataSong?.songs.find((song) => song.id === Number(id));
   audio.src = song.src;
   audio.title = song.title;
-  /*
+
   if (dataSong.currentSong === null || dataSong.currentSong.id !== id) {
     audio.currentTime = 0;
   } else {
     audio.currentTime = dataSong.songCurrentTime;
   }
-  */
   dataSong.currentSong = song;
   audio.play();
 };
